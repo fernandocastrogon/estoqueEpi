f48404a95f26f99f5c9387dd2ef8e13e4788980b
fernandocastrogon <fernandocastrogon@gmail.com>
2022-11-23 00:00:43 -0300

    Utilizando da Classe Scanner para ler o arquivo epis.csv para cadastrar varios Epis(escalável) de uma só vez.

--- src/TesteEstoque.java
+++ src/TesteEstoque.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
@@ -8,9 +9,37 @@ public class TesteEstoque {
     public static void main(String[] args) throws IOException {
 
 
-        Scanner leitor = new Scanner(System.in);
+        Scanner leitorTeclado = new Scanner(System.in);
         //Criando ArrayList para estoque de EPIs
         List<Epi> epis = new ArrayList<>();
+
+
+        Scanner leitorArquivo = new Scanner(new File("epis.csv"));
+        while(leitorArquivo.hasNextLine()){
+            String linha = leitorArquivo.nextLine();
+            System.out.println(linha);
+
+            //Criando scanner e definindo metodo de separacao
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useDelimiter(",");
+
+            //Lendo dados da linha do arquivo epis.csv para criacao de novo Epi
+            String categoria = linhaScanner.next();
+            int ca = linhaScanner.nextInt();
+            String descricao = linhaScanner.next();
+            int quantidadeInicial = linhaScanner.nextInt();
+            int ano = linhaScanner.nextInt();
+            int mes = linhaScanner.nextInt();
+            int dia = linhaScanner.nextInt();
+            epis.add(new Epi(categoria,ca,descricao,quantidadeInicial,LocalDate.of(ano,mes,dia)));
+            System.out.println(epis);
+            linhaScanner.close();
+//            String[] valores = linha.split(",");
+//            System.out.println(valores[0]);
+        }
+        leitorArquivo.close();
+
+
 
 
         //Cadastrando EPIs
@@ -24,14 +53,14 @@ public class TesteEstoque {
         //Cadastrando EPIs em um arquivo .txt
         PrintWriter pw = new PrintWriter("listaEpis.txt");
         //Loop para gravação dos Epis na listaEpis.txt
-        epis.forEach((Epi) -> pw.println(Epi));
+        epis.forEach(pw::println);
         pw.close();
 
 
 
         // Buscando EPI por C.A.
         System.out.println("Digite o CA do EPI que deseja buscar: ");
-        int ca = leitor.nextInt();
+        int ca = leitorTeclado.nextInt();
         //String buscaCa = String.valueOf(ca);
         int posicao = -1;
         for (int i = 0; i < epis.size(); i++) {
@@ -59,7 +88,7 @@ public class TesteEstoque {
         //Imprimindo lista através de lambda
         epis.forEach((Epi) -> System.out.println(Epi.getValidadeCa()));
 
-        leitor.close();
+        leitorTeclado.close();
 
 
 
